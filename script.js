a15d200dad8c1a6496380720f04f659901ae7468
gerijeb <gtpepi@hotmail.com>
2022-01-18 01:52:54 -0300

    Add initial math functions

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const body = document.querySelector("body");
 const container = document.querySelector(".container")
 
-createGrid = () => {
-    
+add = (a, b) => (a + b);
+subtract = (a, b) => (a - b);
+/* sum = (array) => array.reduce((previous, current) => previous + current, 0);
+multiply = (array) => array.reduce((previous, current) => previous * current);
+ */
+multiply = (a, b) => (a * b);
+power = (a, b) => Math.pow(a, b);
+factorial = (n) => {
+  if (n === 0) return 1; else return n * factorial (n -1);
+};
+divide = function (a, b) {
+    if(b === 0) return "Nice try, fucker";
+    return a / b;
 }
 
 
-console.log ();
+
+
