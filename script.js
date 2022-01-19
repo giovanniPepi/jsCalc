d6974328fc10894df71d087e25cdf33c9ec975b7
gerijeb <gtpepi@hotmail.com>
2022-01-18 22:34:59 -0300

    Add cleaning visor functions

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 const body = document.querySelector("body");
 const container = document.querySelector(".container")
 
+const buttons = document.querySelectorAll("[data-number]");
+const operationsP = document.querySelector("#operationsP");
+const historyP = document.querySelector("#historyP");
+
+
 add = (a, b) => (a + b);
 subtract = (a, b) => (a - b);
-/* sum = (array) => array.reduce((previous, current) => previous + current, 0);
-multiply = (array) => array.reduce((previous, current) => previous * current);
- */
+   /* sum = (array) => array.reduce((previous, current) => previous + current, 0);
+   multiply = (array) => array.reduce((previous, current) => previous * current);
+    */
 multiply = (a, b) => (a * b);
 power = (a, b) => Math.pow(a, b);
 factorial = (n) => {
-  if (n === 0) return 1; else return n * factorial (n -1);
+if (n === 0) return 1; else return n * factorial (n -1);
 };
-divide = function (a, b) {
-    if(b === 0) return "Nice try, fucker";
-    return a / b;
+divide = (a, b) => {if (b === 0) alert ("Nice try, fucker"); else return a / b};
+
+fullVisorClean = () => {
+    operationsP.textContent = "";
+    historyP.textContent = "";
+};
+
+visorClean = () => {
+    operationsP.textContent = "";
 }
 
+getEventListeners = () => {
+    const acBtn = document.querySelector("#acBtn");
+    acBtn.addEventListener("click", fullVisorClean);
+    cBtn = document.querySelector("#cBtn");
+    cBtn.addEventListener("click", visorClean);
+}
 
-
+getEventListeners();
 
