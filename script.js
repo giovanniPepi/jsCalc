4a799872d43cb7d2f1b945d5c3df80cf2b912cff
gerijeb <gtpepi@hotmail.com>
2022-01-19 22:56:49 -0300

    Add processNumber() and max 10 character alert

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const body = document.querySelector("body");
 const container = document.querySelector(".container")
 
 const buttons = document.querySelectorAll("[data-number]");
-const operationsP = document.querySelector("#operationsPBlink");
+const operationsP = document.querySelector("#operationsP");
 const historyP = document.querySelector("#historyP");
 
-
+// math
 add = (a, b) => (a + b);
 subtract = (a, b) => (a - b);
    /* sum = (array) => array.reduce((previous, current) => previous + current, 0);
@@ -16,27 +16,39 @@ power = (a, b) => Math.pow(a, b);
 factorial = (n) => {
 if (n === 0) return 1; else return n * factorial (n -1);
 };
-divide = (a, b) => {if (b === 0) alert ("Nice try, fucker"); else return a / b};
+divide = (a, b) => {if (b === 0) alert ("Nice try!"); else return a / b};
 
 fullVisorClean = () => {
     operationsP.textContent = "";
     historyP.textContent = "";
 };
-
 visorClean = () => {
     operationsP.textContent = "";
 }
 getEventListeners = () => {
+    window.addEventListener("click", stopBlinking);
+
     const acBtn = document.querySelector("#acBtn");
     acBtn.addEventListener("click", fullVisorClean);
+
     cBtn = document.querySelector("#cBtn");
     cBtn.addEventListener("click", visorClean);
+
+    buttons.forEach((button) => 
+        button.addEventListener("click", () => processNumber(button.textContent))
+    );
 }
-stopBlinking = () => {
+    stopBlinking = () => {
     const visorPBlink = document.querySelector(".visorPBlink")
-    visorPBlink.setAttribute("class", 'visorP');
-    console.log(visorPBlink);
+    if (visorPBlink) visorPBlink.setAttribute("class", 'visorP');
+}
+function processNumber (number) {
+    if (operationsP.textContent === '.') visorClean();
+    operationsP.textContent.toString().length <= 10? operationsP.textContent += number : alert ("10 number maximum exceeded");
 }
 
-getEventListeners();
 
+
+
+// function activators //
+getEventListeners();
